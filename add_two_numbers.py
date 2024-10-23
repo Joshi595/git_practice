f25be233ce6fab34cd6a501077b11b30ad9ac433
joshi <joshi-vardhan.lella@bmw.de>
2024-10-23 11:11:16 +0200

    mathematics_oops

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -1,5 +1,18 @@
-a = 5
-b = 10
-c = 2
-d = a+b+c
-print(c)
+class mathematics():
+    def __init__(self):
+        pass
+
+    def addition(self, a, b, c):
+        d = a + b + c
+        return d
+    
+    def multiplication(self, a, b, c):
+        e = a * b * c
+        return e
+
+res = mathematics()
+result = res.addition(5, 6, 7)
+product = res.multiplication(8, 6, 7)
+
+print(result)
+print(product)
