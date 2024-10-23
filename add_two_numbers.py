c9ca9b5305476bbee6d2d020245ec76742693c10
joshi <joshi-vardhan.lella@bmw.de>
2024-10-23 11:20:24 +0200

    added new functionalities

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -2,17 +2,19 @@ class mathematics():
     def __init__(self):
         pass
 
-    def addition(self, a, b, c):
-        d = a + b + c
-        return d
+    def addition(self, a, b, c, d):
+        e = a + b + c + d
+        return e
     
     def multiplication(self, a, b, c):
-        e = a * b * c
-        return e
+        f = a * b * c
+        return f
 
 res = mathematics()
 result = res.addition(5, 6, 7)
 product = res.multiplication(8, 6, 7)
+divide = result/product
 
 print(result)
 print(product)
+print(divide)
